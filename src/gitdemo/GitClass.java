2414f72d0558e202d6466cb69ebd0c23e32545d8
Kalai-1998 <nkalai2898@gmail.com>
2025-04-29 00:45:57 +0530

    My new Project

---
+++ src/gitdemo/GitClass.java
@@ -0,0 +1,15 @@
+package gitdemo;
+
+public class GitClass {
+	
+	public void sample() {
+		System.out.println("Hello world");
+	}
+
+	public static void main(String[] args) {
+		GitClass git=new GitClass();
+		git.sample();
+
+	}
+
+}
