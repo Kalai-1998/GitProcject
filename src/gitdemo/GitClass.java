db7bd610c30d6b69fc7b5338c91f3ee3517209b9
Kalai-1998 <nkalai2898@gmail.com>
2025-05-03 14:22:23 +0530

    done My coding

--- src/gitdemo/GitClass.java
+++ src/gitdemo/GitClass.java
@@ -5,11 +5,15 @@ public class GitClass {
 	public void sample() {
 		System.out.println("Hello world");
 	}
+	public void Sample1() {
+		System.out.println("Welcome to GIthub");
+	}
 
 	public static void main(String[] args) {
 		GitClass git=new GitClass();
 		git.sample();
 
 	}
+	
 
 }
